fbee079f223ed40d19a172d46ecd241c2df3ecb9
rummanwaqar <rumman.waqar05@gmail.com>
2019-01-08 16:32:01 -0700

    Kalman filter implemented

---
+++ include/kalman_filter.h
@@ -0,0 +1,66 @@
+#ifndef _EKF_KALMAN_FILTER_H_
+#define _EKF_KALMAN_FILTER_H_
+
+#include "Eigen/Dense"
+
+namespace carnd_ekf {
+  class KalmanFilter {
+  public:
+    /*
+     * Constructor
+     */
+    KalmanFilter();
+
+    /*
+     * Destructor
+     */
+    virtual ~KalmanFilter() = default;
+
+    /*
+     * Initialize kalman filter
+     * @param x_in Initial state
+     * @param P_in Initial state covariance
+     * @param F_in Transition matrix
+     * @param H_in Measurement matrix
+     * @param R_in Measurement covariance matrix
+     * @param Q_in Process covariance matrix
+     */
+    void init(Eigen::VectorXd& x_in, Eigen::MatrixXd& P_in, Eigen::MatrixXd& F_in,
+              Eigen::MatrixXd& H_in, Eigen::MatrixXd& R_in, Eigen::MatrixXd& Q_in);
+
+    /*
+     * predicts the state and state covariance using the process model
+     * @param delta_T time between k and k+1 in seconds
+     */
+    void predict(double delta_T = 1.0);
+
+    /*
+     * updates the state using measurement and kalman equations
+     */
+    void update(const Eigen::VectorXd& z);
+
+  public:
+    // state vector
+    Eigen::VectorXd x_;
+
+    // state covariance
+    Eigen::MatrixXd P_;
+
+    // state transition matrix
+    Eigen::MatrixXd F_;
+
+    // process covariance
+    Eigen::MatrixXd Q_;
+
+    // measurement matrix
+    Eigen::MatrixXd H_;
+
+    // measurement covariance
+    Eigen::MatrixXd R_;
+
+    // identity matrix
+    Eigen::MatrixXd I_;
+  }; // class KalmanFilter
+} // namespace carnd_ekf
+
+#endif  // _EKF_KALMAN_FILTER_H_
---
+++ src/kalman_filter.cpp
@@ -0,0 +1,45 @@
+#include "kalman_filter.h"
+
+using namespace carnd_ekf;
+
+KalmanFilter::KalmanFilter() {
+  I_ = Eigen::MatrixXd::Identity(2, 2);
+}
+
+void KalmanFilter::init(Eigen::VectorXd& x_in, Eigen::MatrixXd& P_in,
+                        Eigen::MatrixXd& F_in, Eigen::MatrixXd& H_in,
+                        Eigen::MatrixXd& R_in, Eigen::MatrixXd& Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::predict(double delta_T) {
+  // prior
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
+}
+
+void KalmanFilter::update(const Eigen::VectorXd& z) {
+  // residual
+  Eigen::VectorXd y = z - H_ * x_;
+  // subexpression
+  Eigen::MatrixXd PHT = P_ * H_.transpose();
+  // system uncertainity
+  Eigen::MatrixXd S = H_ * PHT + R_;
+  // kalman gain
+  Eigen::MatrixXd K = PHT * S.inverse();
+  // posterior
+  x_ = x_ + (K * y);
+
+  /*
+   * P = (I - KH) * P is numerically unstable because (I - KH) float error can
+   * make P non-symmetric
+   * equivalent but stable derivation P = (I-KH)P(I-KH)' + KRK'
+   */
+  Eigen::MatrixXd I_KH = I_ - K * H_;
+  P_ = I_KH * P_ * I_KH.transpose() + K * R_ * K.transpose();
+}
